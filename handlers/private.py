00a6a4bfb2cc298331f0978311bc87937648318a
TGBotSzK <77600757+ZauteKm@users.noreply.github.com>
2021-04-17 20:42:36 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -1,4 +1,4 @@
-from pyrogram import Client
+from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 from config import BOT_NAME as bn
@@ -7,26 +7,46 @@ from helpers.filters import other_filters2
 
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
+    await message.reply_sticker("CAACAgQAAx0CTv65QgABBfJlYF6VCrGMm6OJ23AxHmD6qUSWESsAAhoQAAKm8XEeD5nrjz5IJFYeBA")
     await message.reply_text(
-        f"""I am **{bn}** !!
-<b>I let you Play Music in your Group's Voice chat.</b>
-The commands I currently support are:
-⚜️ /play - __Plays the replied audio file or YouTube video through link.__
-⚜️ /pause - __Pause Voice Chat Music.__
-⚜️ /resume - __Resume Voice Chat Music.__
-⚜️ /skip - __Skips the current Music Playing In Voice Chat.__
-⚜️ /stop - __Clears The Queue as well as ends Voice Chat Music.__
+        f"""**Hey, I'm **{bn}** 🎵
+
+I can play music in your group's voice call. Developed by [Jason](https://t.me/ImJanindu).
+
+Add me to your group and play music freely!**
         """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Group 💬", url="https://t.me/ZauTeSupport"
+                        "🛠 Helpful Command 🛠", url="https://telegra.ph/VC-Music-Player-Bot-Helpful-Commands-04-17")
+                  ],[
+                    InlineKeyboardButton(
+                        "💬 Group", url="https://t.me/ZauTeSupport"
                     ),
                     InlineKeyboardButton(
-                        "Channel 📣", url="https://t.me/ZauTeKm"
+                        "🔊 Channel", url="https://t.me/ZauTeKm"
                     )
+                ],[ 
+                    InlineKeyboardButton(
+                        "➕ Add To Your Group ➕", url="https://t.me/zK_GvCBot?startgroup=true"
+                    )]
+            ]
+        ),
+     disable_web_page_preview=True
+    )
+
+@Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
+async def gstart(_, message: Message):
+      await message.reply_text("""**Group Music Player Online ✅**""",
+      reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🔊 Channel", url="https://t.me/ZauTeKm")
                 ]
             ]
         )
-    )
+   )
+
+
