c4c078809084b8c801bb80257dbc1aad81b5814c
Ahmadsalim99 <83236423+Ahmadsalim99@users.noreply.github.com>
2021-04-29 16:47:00 +0300

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -21,11 +21,7 @@ async def play(_, message: Message):
     audio = (message.reply_to_message.audio or message.reply_to_message.voice) if message.reply_to_message else None
     url = get_url(message)
 
-    if audio:
-        if round(audio.duration / 60) > DURATION_LIMIT:
-            raise DurationLimitError(
-                f"**{bn} :-**  مقطع الفيديو هذا أطول من  {DURATION_LIMIT} minute(s) لايمكن تحميله \n يسمح بالمقطع ان يكون {audio.duration / 60} minute(s)"
-            )
+  
 
         file_name = get_file_name(audio)
         file_path = await converter.convert(
